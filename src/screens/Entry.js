03c6994996747d7fd0c7417942d2c52c9121ec44
Spencer <spencerlevitt2022@u.northwestern.edu>
2020-09-29 17:23:13 -0500

    Sectioned options

--- src/screens/Entry.js
+++ src/screens/Entry.js
@@ -1,53 +1,71 @@
-import React from 'react';
-import { StyleSheet, SafeAreaView, ScrollView, Text, View } from 'react-native';
-import Form from '../components/Form';
+import React from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  View,
+  SectionList,
+} from "react-native";
+import Form from "../components/Form";
+import exampleData from "../../exampleData.json";
+
+const sectionData = exampleData.options.map((option) => ({
+  title: option,
+  data: ["scores"],
+}));
 
 const Entry = ({ route }) => {
-
-    var criteria = ["Writing", "VFX", "Acting", "Music", "Action"]
-    var fields = []
-
-    for (var i = 0; i < criteria.length; i++) {
-        fields.push(
-            <View>
-                <Text>{criteria[i]}</Text>
-                <Form.Field
-                    name={criteria[i]}
-                    placeholder={criteria[i]}
-                />
-            </View>
-        )
-    }
-    return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView>
-                <Text>PROMPT</Text>
-                <Text>What Star Wars movie are we watching?</Text>
-                <Form
-                    initialValues={{
-                        id: null,
-                        meets: null,
-                        title: null,
-                    }}>
-                    {fields}
-
-                </Form>
-            </ScrollView>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      <SectionList
+        sections={sectionData}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(index) => `${index}`}
+        renderItem={({ section: { data } }) => (
+          <Form
+            initialValues={{
+              id: null,
+              meets: null,
+              title: null,
+            }}
+          >
+            {exampleData.criteria.map((item) => (
+              <View key={item}>
+                <Text>{item}</Text>
+                <Form.Field name={item} placeholder={`Enter ${item}`} />
+              </View>
+            ))}
+          </Form>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <View style={styles.header}>
+            <Text>{title}</Text>
+          </View>
+        )}
+      />
+      <Text>PROMPT</Text>
+      <Text>What Star Wars movie are we watching?</Text>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        // flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    field: {
-        display: 'flex',
-        flexDirection: 'row'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  field: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  header: {
+    backgroundColor: "grey",
+    padding: 10,
+    borderRadius: 10,
+  },
 });
 
-export default Entry
+export default Entry;
