2f7d4f3ff6b9a435243c4313f8e3a30442956c5a
nelly-ok <okunlolanelson@gmail.com>
2020-10-13 19:38:49 -0500

    results algorithm efficiency refactor and results displays total

--- utils/winner.js
+++ utils/winner.js
@@ -1,48 +1,56 @@
 const winner = (pollData) => {
-  //pollData.votes[0].options[0].scores[criteria];
-
+  
+  //creates an object that maps options to their scores along with a total value
   let sum = {};
+  let lead = '';
+  let max = 0;
   let data = Object.assign({}, pollData);
   let votes = Object.values(data.scores)
+  //for each choices of each user's vote (based on db scheme)
   for (let vote of votes) {
     for (const [choice, scores] of Object.entries(vote)) {
-
+      //first time accessing option? set it to that vote and create a total value
       if (!sum[choice]) {
         sum[choice] = Object.assign({}, scores);
         const tally = Object.values(scores);
-        sum[choice].total = tally.reduce((a, b) => {
+        sum[choice].Total = tally.reduce((a, b) => {
           return a + b;
         }, 0);
-      } else {
+
+        //check if we have a new winner
+        if ((sum[choice].Total) > max) {
+          max = (sum[choice].Total);
+          lead = choice;
+        }
+        
+
+      } 
+      //else aggregate
+      else {
         for (let criteria of data.criteria) {
           sum[choice][criteria] += scores[criteria]
-          sum[choice].total += scores[criteria]
+          sum[choice].Total += scores[criteria]
+        }
+
+        //check if we have a new winner
+        if ((sum[choice].Total) > max){
+          max = (sum[choice].Total);
+          lead = choice;
         }
       }
     }
   };
 
-  //**Does not account for ties**
-  let lead = '';
-  let max = 0;
-  for (const [key, value] of Object.entries(sum)) {
-    if (value.total > max) {
-      max = value.total;
-      lead = key;
-    }
-  };
-
+  //push the winner to the top of the lisr
   let res = [getResults(lead, sum, data.criteria)]
 
+  //push everything else
   for (let option of data.options) {
     if (option != lead) {
       res.push(getResults(option, sum, data.criteria))
     }
   }
-
   return res;
-
-
 
 };
 
@@ -54,11 +62,14 @@ const getResults = (option, sum, criteria) => {
     scores.push(sum[option][crit]);
   };
 
+  //might be a more efficient way to do this but this adds the total to the results graph
+  scores.push(sum[option].Total)
+
 
   return {
     title: option,
     barData: {
-      labels: criteria,
+      labels: [...criteria, 'Total'],
       datasets: [{
         data: scores,
       }, ],
