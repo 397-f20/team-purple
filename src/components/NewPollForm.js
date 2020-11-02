da180f78c0146b75787a9d5e79b373c8cdda65d6
Gus Umbelino <gumbelin@gmail.com>
2020-11-02 17:01:05 -0600

    checking for repeated options/criteria done

--- src/components/NewPollForm.js
+++ src/components/NewPollForm.js
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
 import { SafeAreaView, ScrollView, Text, Button } from "react-native";
 import Form from "../components/Form";
+import * as Yup from "yup";
 
 import { firebase } from "../../utils/firebase";
 
 import randomWords from "random-words";
+
+const validationSchema = Yup.object().shape({
+  // id: Yup.string()
+  //   .required()
+  //   .matches(/(F|W|S)\d{3,}/, "Must be a term and 3-digit number")
+  //   .label("ID"),
+  // meets: Yup.string()
+  //   .required()
+  //   .matches(
+  //     /(M|Tu|W|Th|F)+ +\d\d?:\d\d-\d\d?:\d\d/,
+  //     "Must be weekdays followed by start and end time"
+  //   )
+  //   .label("Meeting times"),
+  prompt: Yup.string().required().label("Prompt"),
+  // options: Yup.mixed().notOneOf(["op1"]).defined(),
+  // criteria: ,
+});
 
 // FIXME: A text node cannot be a child of a <View>
 const NewPollForm = ({ navigation, route }) => {
@@ -12,6 +30,8 @@ const NewPollForm = ({ navigation, route }) => {
   const [prompt, setPrompt] = useState("Which movie are we watching?");
   const [options, setOptions] = useState(["movie 1", "movie 2"]);
   const [criteria, setCriteria] = useState(["writing", "vfx", "acting"]);
+  const [hasError, setHasError] = useState(false);
+  const [errorMessage, setErrorMesssage] = useState("");
 
   const addOption = () => {
     setOptions([...options, ""]);
@@ -30,7 +50,29 @@ const NewPollForm = ({ navigation, route }) => {
   };
 
   async function handleSubmit(values) {
+    console.log(values);
     const { prompt, options, criteria } = values;
+
+    const hasRepeats = (array) => {
+      let seen = {};
+      for (let item of array) {
+        if (!seen[item]) seen[item] = true;
+        else return true;
+      }
+    };
+
+    if (hasRepeats(options)) {
+      setErrorMesssage("No repeated options!");
+      setHasError(true);
+      return;
+    }
+
+    if (hasRepeats(criteria)) {
+      setErrorMesssage("No repeated criteria!");
+      setHasError(true);
+      return;
+    }
+
     const roomCode = randomWords();
     console.log(roomCode);
     const newPoll = { prompt, options, criteria, roomCode, count: 0 };
@@ -53,6 +95,7 @@ const NewPollForm = ({ navigation, route }) => {
             options: options,
             criteria: criteria,
           }}
+          validationSchema={validationSchema}
           onSubmit={(values) => handleSubmit(values)}
         >
           <Text>Prompt</Text>
@@ -66,8 +109,8 @@ const NewPollForm = ({ navigation, route }) => {
           <Text>Options</Text>
           {options.map((op, index) => (
             <Form.Field
-              key={`options.${index}`}
-              name={`options.${index}`}
+              key={`options[${index}]`}
+              name={`options[${index}]`}
               leftIcon="calendar-range"
               placeholder={op}
               autoCapitalize="none"
@@ -82,8 +125,8 @@ const NewPollForm = ({ navigation, route }) => {
           <Text>Criteria</Text>
           {criteria.map((crit, index) => (
             <Form.Field
-              key={`criteria.${index}`}
-              name={`criteria.${index}`}
+              key={`criteria[${index}]`}
+              name={`criteria[${index}]`}
               leftIcon="calendar-range"
               placeholder={crit}
               autoCapitalize="none"
@@ -95,8 +138,8 @@ const NewPollForm = ({ navigation, route }) => {
           {criteria.length > 1 && (
             <Button onPress={() => removeCriteria()} title="Remove criteria" />
           )}
+          {<Form.ErrorMessage error={errorMessage} visible={hasError} />}
           <Form.Button title={"Create"} />
-          {/* {<Form.ErrorMessage error={submitError} visible={true} />} */}
         </Form>
       </ScrollView>
     </SafeAreaView>
