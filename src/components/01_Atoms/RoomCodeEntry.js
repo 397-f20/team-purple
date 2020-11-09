11d7f2e77f5e5d18984aa99597d27697e825cbd7
Spencer <spencerlevitt2022@u.northwestern.edu>
2020-11-09 12:13:17 -0600

    Restyled home page

--- src/components/01_Atoms/RoomCodeEntry.js
+++ src/components/01_Atoms/RoomCodeEntry.js
@@ -1,35 +1,44 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Text, Button, TextInput, View } from "react-native";
-import { fonts } from '../../styles/all_styles';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  Button,
+  TextInput,
+  View,
+} from "react-native";
+import { fonts, colors } from "../../styles/all_styles";
 
 const RoomCodeEntry = ({ roomCode, setRoomCode, navigation }) => {
-
-    return (
-        <View style={styles.RoomCodeEntry}>
-            {/* entry for room code */}
-            <Text style={[fonts.h1, { marginBottom: 15 }]}>Room Code:</Text>
-            <TextInput
-                style={styles.input}
-                onSubmitEditing={() => navigation.navigate("Entry", {roomCode })}
-                onChangeText={(text) => setRoomCode(text)}
-                value={roomCode}
-            />
-        </View>
-    );
+  return (
+    <View style={styles.RoomCodeEntry}>
+      {/* entry for room code */}
+      <Text style={[fonts.h1, { marginBottom: 15, alignSelf: "flex-start" }]}>
+        Poll Key:
+      </Text>
+      <TextInput
+        style={styles.input}
+        onSubmitEditing={() => navigation.navigate("Entry", { roomCode })}
+        onChangeText={(text) => setRoomCode(text)}
+        value={roomCode}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    RoomCodeEntry:{
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    input: {
-        height: 40,
-        width: '80%',
-        backgroundColor: 'white'
-    },
+  RoomCodeEntry: {
+    marginHorizontal: "5%",
+    justifyContent: "center",
+    width: "90%",
+  },
+  input: {
+    height: 40,
+    width: "100%",
+    backgroundColor: "white",
+    borderRadius: 10,
+    paddingLeft: 10,
+  },
 });
 
 export default RoomCodeEntry;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,19 +6,23 @@ import {
   Button,
   View,
   Image,
+  TouchableOpacity,
 } from "react-native";
-import { fonts } from "../styles/all_styles";
+import { fonts, colors } from "../styles/all_styles";
 import RoomCodeEntry from "../components/01_Atoms/RoomCodeEntry";
 
 const Home = ({ route, navigation }) => {
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
-        <Button
-          data-cy="/newPollButton"
-          title="New Poll"
-          onPress={() => navigation.navigate("NewPoll")}
-        />
+        <View style={{ marginRight: 20 }}>
+          <Button
+            data-cy="/newPollButton"
+            title="New Poll"
+            color={colors.primaryColor}
+            onPress={() => navigation.navigate("NewPoll")}
+          />
+        </View>
       ),
     });
   }, [navigation]);
@@ -26,15 +30,24 @@ const Home = ({ route, navigation }) => {
   const [roomCode, setRoomCode] = React.useState("");
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.instructions}>
-        <Text style={[fonts.p, { fontWeight: 'bold' }]}>Instructions:</Text>
-        <Text style={[fonts.p]}>
-          Enter the room code below or start a new poll!
-      </Text>
-      </View>
-
-
       <Image style={styles.logo} source={require("../../assets/logo.png")} />
+
+      <View style={styles.instructions}>
+        <Text
+          style={[
+            fonts.p,
+            { fontWeight: "bold", marginBottom: 15, fontSize: 20 },
+          ]}
+        >
+          Welcome!
+        </Text>
+        <Text style={[fonts.p, { fontSize: 13 }]}>
+          Use Tiebreaker to ensure all members in the group have their voice
+          heard when making decisions. If the prompt has already been created,
+          enter the poll code below. If not, create a new poll by tapping the
+          button in the top right.
+        </Text>
+      </View>
 
       {/* entry for room code */}
       <RoomCodeEntry
@@ -42,7 +55,6 @@ const Home = ({ route, navigation }) => {
         setRoomCode={setRoomCode}
         navigation={navigation}
       />
-
     </SafeAreaView>
   );
 };
@@ -52,11 +64,15 @@ const styles = StyleSheet.create({
     flex: 1,
     display: "flex",
     alignItems: "center",
-    // justifyContent: "center",
+    marginTop: 50,
   },
   instructions: {
-    marginTop: '10px',
-    marginBottom: '200px'
+    marginTop: "10px",
+    marginBottom: "50px",
+    marginHorizontal: "5%",
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
   },
   input: {
     height: 40,
