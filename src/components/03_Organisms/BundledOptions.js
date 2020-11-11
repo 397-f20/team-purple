1c847ecf85a79fad03822eac74fe5dc310a1570e
Gus Umbelino <gumbelin@gmail.com>
2020-11-10 19:40:25 -0600

    removed Formik from NewPoll

--- src/components/03_Organisms/BundledOptions.js
+++ src/components/03_Organisms/BundledOptions.js
@@ -14,66 +14,78 @@ import { fonts, colors } from "../../styles/all_styles";
 // data for the flatlist
 const DATA = [
   {
-    title: 'Movie',
-    prompt: 'What movie should we watch?',
-    criteria: ['VFX', 'Soundtrack', 'Writing', 'Acting']
+    title: "Movie",
+    prompt: "What movie should we watch?",
+    criteria: ["VFX", "Soundtrack", "Writing", "Acting"],
   },
   {
-    title: 'Restaurant',
-    prompt: 'What restaurant should we eat at?',
-    criteria: ['Cost', 'Vibe', 'Food Quality', 'Distance']
+    title: "Restaurant",
+    prompt: "What restaurant should we eat at?",
+    criteria: ["Cost", "Vibe", "Food Quality", "Distance"],
   },
   {
-    title: 'Tech Stack',
-    prompt: 'What should we use for our frontend or backend tech stack?',
-    criteria: ['Documentation', 'Prior Knowledge', 'Feasability']
+    title: "Tech Stack",
+    prompt: "What should we use for our frontend or backend tech stack?",
+    criteria: ["Documentation", "Prior Knowledge", "Feasability"],
   },
   {
-    title: 'Contact Platform',
-    prompt: 'How will our team communicate for this project?',
-    criteria: ['Prior Experience', 'Check Frequency', 'Feasability']
-  }
-]
+    title: "Contact Platform",
+    prompt: "How will our team communicate for this project?",
+    criteria: ["Prior Experience", "Check Frequency", "Feasability"],
+  },
+];
 
-const BundledOptions = ({onPress}) => {
+const BundledOptions = ({ onPress, activeBundle }) => {
   return (
     <View style={styles.container}>
-      <FlatList 
-        data={DATA} 
-        keyExtractor={(item) => item.title} 
+      <FlatList
+        data={DATA}
+        keyExtractor={(item) => item.title}
         horizontal
-        contentContainerStyle={{paddingLeft: '10px'}}
+        contentContainerStyle={{ paddingLeft: "10px" }}
         showsHorizontalScrollIndicator={false}
-        renderItem={({ item }) => 
-          <TouchableOpacity style={styles.card} onPress={()=> onPress(item)}>
+        renderItem={({ item, index }) => (
+          <TouchableOpacity
+            style={activeBundle == index ? styles.cardActive : styles.card}
+            onPress={() => onPress(item, index)}
+          >
             <Text style={styles.cardTitle}>{item.title}</Text>
-          </TouchableOpacity>}
+          </TouchableOpacity>
+        )}
       />
     </View>
   );
 };
 
+const cardBase = {
+  width: "100px",
+  height: "50px",
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: "5px",
+  marginRight: "10px",
+  padding: "5px",
+};
+
 const styles = StyleSheet.create({
   container: {
     marginVertical: 20,
-    justifyContent: 'center'
+    justifyContent: "center",
   },
   card: {
+    ...cardBase,
     backgroundColor: colors.secondaryColor,
-    width: '100px',
-    height: '50px',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: '5px',
-    marginRight: '10px',
-    padding: '5px',
+  },
+  cardActive: {
+    ...cardBase,
+    backgroundColor: colors.primaryColor,
   },
   cardTitle: {
-    textAlign: 'center',
-    color: 'white'
-  }
+    textAlign: "center",
+    color: "white",
+  },
 });
 
 export default BundledOptions;
