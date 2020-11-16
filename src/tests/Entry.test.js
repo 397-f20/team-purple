3a284aac05c4dd5d999f358bb22c0ea6a6d5b9a8
Spencer <spencerlevitt2022@u.northwestern.edu>
2020-11-15 18:51:37 -0600

    Add test for mocking state

--- src/tests/Entry.test.js
+++ src/tests/Entry.test.js
@@ -2,15 +2,13 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Entry from "../screens/Entry";
 
-
-
 describe("<Entry />", () => {
   it("has 3 child", () => {
     const tree = renderer.create(<Entry />).toJSON();
     expect(tree.children.length).toBe(3);
   });
 
-  /*let data = {
+  let data = {
     count: 8,
     criteria: ["Comfortability", "Package Ecosystem", "Documentation"],
     options: ["React", "Vue"],
@@ -18,21 +16,22 @@ describe("<Entry />", () => {
     roomCode: "breathing",
   };
   const handleData = (data, callback) => {
-    if (data != null && values == null) {
-      const mappedCriteria = data.criteria.map(function (key) {
-        return [key, 0];
-      });
-      const valuesObj = Object.fromEntries(mappedCriteria);
-      const mappedOptions = data.options.map(function (key) {
-        return [key, { ...valuesObj }];
-      });
-      const optionsObj = Object.fromEntries(mappedOptions);
-      callback(optionsObj);
-    }
+    const mappedCriteria = data.criteria.map(function (key) {
+      return [key, 0];
+    });
+    const valuesObj = Object.fromEntries(mappedCriteria);
+    const mappedOptions = data.options.map(function (key) {
+      return [key, { ...valuesObj }];
+    });
+    const optionsObj = Object.fromEntries(mappedOptions);
+    callback(optionsObj);
   };
 
   const mockCallback = jest.fn((data) => data);
-  handleData(data, mockCallback);
 
-  console.log(mockCallback.mock.results[0].value);*/
+  it("organizes data from server", () => {
+    handleData(data, mockCallback);
+    const state = mockCallback.mock.results[0].value;
+    expect(state.React["Comfortability"]).toBe(0);
+  });
 });
