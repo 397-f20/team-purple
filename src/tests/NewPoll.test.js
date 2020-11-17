33e15c498b492625d3f39c7dd53d2f5f2b878ea2
Gus Umbelino <gumbelin@gmail.com>
2020-11-17 09:29:49 -0600

    added 1 newPoll test for validatePollForm

--- src/tests/NewPoll.test.js
+++ src/tests/NewPoll.test.js
@@ -2,11 +2,30 @@ import React from "react";
 import renderer from "react-test-renderer";
 import NewPoll from "../screens/NewPoll";
 
-jest.mock('react-dom')
+import validatePollForm from "../../utils/pollValidation";
 
 describe("<NewPoll />", () => {
   it("has 1 child", () => {
     const tree = renderer.create(<NewPoll />).toJSON();
     expect(tree.children.length).toBe(1);
+  });
+
+  const mockPrompt = "";
+  const mockOptions = [];
+  const mockCriteria = [];
+
+  const handleData = (callback) => {
+    const pollEval = validatePollForm(mockPrompt, mockOptions, mockCriteria);
+    callback(pollEval);
+  };
+
+  const mockCallback = jest.fn((data) => data);
+
+  it("catches empty prompt", () => {
+    handleData(mockCallback);
+    const errorMessage = mockCallback.mock.results[0].value;
+    expect(errorMessage.message).toBe(
+      "Prompt should be longer than 5 characters"
+    );
   });
 });
