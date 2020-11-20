636860fed27cfcb374a97e3cbfb184e5304017f1
nelly-ok <okunlolanelson@gmail.com>
2020-11-19 18:56:09 -0600

    refactor/cleaned

--- utils/winner.js
+++ utils/winner.js
@@ -1,28 +1,30 @@
 const winner = (pollData) => {
-  //creates an object that maps each option to an object that maps each criteria to a total score
+  //creates an object that each score for each criteria for each option
   let sum = {};
   let critVoteCount = {}
-  let data = {...pollData};
+  let data = {
+    ...pollData
+  };
   let votes = Object.values(data.scores);
 
   //for each user's vote (based on db scheme)
   for (let vote of votes) {
     for (const [option, scores] of Object.entries(vote)) {
-      //first time accessing option? set the sum of that optiion to this individual vote and create a total value
+      //first time seeing option? set the sum of that option to this individual vote. keep a tally of nonzero votes. 
       if (!sum[option]) {
-        sum[option] = Object.assign({}, scores);
-        critVoteCount[option] = Object.assign({}, scores);
-        const tally = Object.values(scores);
-        sum[option].Total = tally.reduce((a, b) => {
-          return a + b;
-        }, 0);
+        sum[option] = {
+          ...scores
+        };
+        critVoteCount[option] = {
+          ...scores
+        };
 
         for (const [criteria, score] of Object.entries(scores)) {
           critVoteCount[option][criteria] = (score == 0) ? 0 : 1
         }
-       
+
       }
-      //else its not your first time accessing so just add on
+      //else its not your first time accessing so just add on (for each criteria). keep track of nonzero votes
       else {
         for (let criteria of data.criteria) {
           sum[option][criteria] += scores[criteria];
@@ -33,9 +35,7 @@ const winner = (pollData) => {
     }
   }
 
-  console.log(critVoteCount)
-
-  //calls getResult with each option, then sort the list
+  //for each option call getResult, then sort the list by highest overall
   let res = data.options.map((option) =>
     getResults(option, sum, data.criteria, critVoteCount)
   );
@@ -43,44 +43,32 @@ const winner = (pollData) => {
 
   // account for if multiple winners / tie
   var winVoteCount = res[0].overall
-  for (var i=0; i<res.length; i++){
-    // mark it as a winner if it has the same score
-    if (res[i].overall == winVoteCount){
-      res[i].win = true
-    }
-    // don't need to check the rest of the list if it doesnt match
-    else{
+  for (let option of res) {
+    if (option.overall == winVoteCount) { // mark it as a winner if it has the same score as the wineer
+      option.win = true
+    } else { // don't need to check the rest of the list if it doesnt match
       break
     }
   }
   return res;
 };
 
+
 // scale to 5 star rating and return in a format to be used by UI
 const getResults = (option, sum, criteria, voteCounts) => {
+
   // puts scores total in the order of the criteria array
   let scores = [];
   for (let crit of criteria) {
     scores.push(sum[option][crit]);
   }
 
-  scores.push(sum[option].Total);
+  //scale each Criteria overall to a 5 point scale - only accounting for nonZeroVotes
+  const scaledCriteriaRating = scores.map((item, index) => 
+    scaleBetween(item, 1, 5, 1 * voteCounts[option][criteria[index]], 5 * voteCounts[option][criteria[index]]));
 
-  // const scaledOverallRating = scaleBetween(
-  //   scores[scores.length - 1],
-  //   1,
-  //   5,
-  //   1 * voteCount * criteria.length,
-  //   5 * voteCount * criteria.length
-  // );
-  // console.log(scores[scores.length - 1], scaledOverallRating);
-
-  const scaledCriteriaRating = scores
-    .slice(0, -1)
-    .map((item, index) => scaleBetween(item, 1, 5, 1 * voteCounts[option][criteria[index]], 5 * voteCounts[option][criteria[index]]));
-
-    const scaledOverallRating = scaledCriteriaRating.reduce((a, b) => a + b, 0) / scaledCriteriaRating.length
-
+  //Overall is an average of all criterias=
+  const scaledOverallRating = scaledCriteriaRating.reduce((a, b) => a + b, 0) / scaledCriteriaRating.length
 
   return {
     title: option,
@@ -94,9 +82,9 @@ const getResults = (option, sum, criteria, voteCounts) => {
 
 // scale to 5 star rating
 const scaleBetween = (unscaledNum, minAllowed, maxAllowed, min, max) => {
-  return (
-    ((maxAllowed - minAllowed) * (unscaledNum - min)) / (max - min) + minAllowed
-  );
+  //added because this returns NaN (dividing by 0) if max and min is 0 which occurs when there are no votes for a criteria
+  let res = (max - min == 0) ? 0 : ((maxAllowed - minAllowed) * (unscaledNum - min)) / (max - min) + minAllowed;
+  return res;
 }
 
 export default winner;
